f65f9baf8743711920d06c3255cfec5aef44fd59
Michał Kuś <kusmichal06@gmail.com>
2025-08-12 21:01:13 +0200

    Refactor FriendsCard component: reorganize JSX structure for better readability and maintainability.

--- src/components/profile/FriendsCard.tsx
+++ src/components/profile/FriendsCard.tsx
@@ -10,25 +10,27 @@ const FriendsCard: React.FC<FriendsCardProps> = ({backCallback: backCallback}) =
         <div className="profile-header friends-card">
             <h2>Friends</h2>
             <div className="profile-info-container">
-                <div className="info-row">
-                    <span className="friend-data">Friend 1</span>
-                    <span className={false ? "friend-online" : "friend-offline"}>
-                        <FontAwesomeIcon icon={faDumbbell} size='lg'/>
-                    </span>
+                <div className='profile-info'>
+                    <div className="info-row">
+                        <span className="friend-data">Friend 1</span>
+                        <span className={false ? "friend-online" : "friend-offline"}>
+                            <FontAwesomeIcon icon={faDumbbell} size='lg'/>
+                        </span>
+                    </div>
+                    <div className="info-row">
+                        <span className="friend-data">Friend 2</span>
+                        <span className={true ? "friend-online" : "friend-offline"}>
+                            <FontAwesomeIcon icon={faDumbbell} size='lg'/>
+                        </span>
+                    </div>
+                    <div className="info-row">
+                        <span className="friend-data">Friend 3</span>
+                        <span className={false ? "friend-online" : "friend-offline"}>
+                            <FontAwesomeIcon icon={faDumbbell} size='lg'/>
+                        </span>
+                    </div>
+                    <button className="back-btn" onClick={() => backCallback()}>Back</button>
                 </div>
-                <div className="info-row">
-                    <span className="friend-data">Friend 2</span>
-                    <span className={true ? "friend-online" : "friend-offline"}>
-                        <FontAwesomeIcon icon={faDumbbell} size='lg'/>
-                    </span>
-                </div>
-                <div className="info-row">
-                    <span className="friend-data">Friend 3</span>
-                    <span className={false ? "friend-online" : "friend-offline"}>
-                        <FontAwesomeIcon icon={faDumbbell} size='lg'/>
-                    </span>
-                </div>
-                <button className="back-btn" onClick={() => backCallback()}>Back</button>
             </div>
         </div>
     );
